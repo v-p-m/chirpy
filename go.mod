33959626b9ad6aada98a9caa1473eee2f1d4b970
Teuska <demac@kapsi.fi>
2025-09-13 13:10:36 +0300

    GET: /app, /app/assets, /healthz endpoints

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/v-p-m/chirpy
+
+go 1.23.4
--- main.go
+++ main.go
@@ -8,21 +8,28 @@ import (
 
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	// Create a new request multiplexer (router) to handle HTTP requests.
 	// This allows you to register multiple routes and handlers.
 	mux := http.NewServeMux()
 
 	// Create a file server to serve static files from the current directory (".").
 	// This will serve files like index.html, CSS, JS, etc.
-	filesrv := http.FileServer(http.Dir("."))
+	filesrv := http.FileServer(http.Dir(filepathRoot))
 
 	// Register the file server as the handler for the root path ("/").
 	// This means all requests to the root and its subpaths will be handled by the file server.
-	mux.Handle("/", filesrv)
+	mux.Handle("/app/", http.StripPrefix("/app", filesrv))
+
+	// Register the handler function for the "/healthz" endpoint.
+	// This endpoint is typically used for health checks.
+	mux.HandleFunc("/healthz", handlerHealthz)
 
 	// Create a new HTTP server instance with custom settings.
 	server := &http.Server {
-		Addr:		":8080",	// Set the server to listen on port 8080 for all network interfaces.
+		Addr:		":" + port,	// Set the server to listen on port 8080 for all network interfaces.
 		Handler:	mux,		// Use the ServeMux as the server's request handler.
 	}
 
@@ -33,4 +40,12 @@ func main() {
 		// If the server fails to start, log the error and exit.
 		log.Fatalf("Server failed: %v", err)
 	}
+}
+
+// handlerHealthz responds to health check requests with a 200 OK status.
+// It sets the response content type to plain text and writes the status text.
+func handlerHealthz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
